48fc0484c4acb7f758042ec49e2610a3d07c4c8a
ebrewe <eric.brewer@sandboxww.com>
2016-11-30 14:58:20 -0500

    added bootstrap, vuex, vue-router

---
+++ src/main.js
@@ -0,0 +1,11 @@
+import Vue from 'vue'
+import App from './components/App/App.vue'
+
+import './style/main.scss'
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  template: '<App/>',
+  components: { App }
+})
